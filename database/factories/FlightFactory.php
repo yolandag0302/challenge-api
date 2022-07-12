a582fa795b2abcc801fb29295eff466e81bc7228
Yolanda <yolandag0302@gmail.com>
2022-07-11 23:57:35 -0500

    Adding Flight Factory and Flight Seeder to seed my database with data using seed classes. Adding test.http to get data.

---
+++ database/factories/FlightFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flight>
+ */
+class FlightFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'origin_city' => fake()->city(),
+            'destination_city' => fake()->city(),
+            'airline_name' => fake()->company(),
+        ];
+    }
+}
