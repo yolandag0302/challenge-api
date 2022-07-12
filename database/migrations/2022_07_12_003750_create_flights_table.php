4fdc42817bb5391832540a2429c4bc3770852279
Yolanda <yolandag0302@gmail.com>
2022-07-11 19:58:24 -0500

    Creating Flight model and migration

---
+++ database/migrations/2022_07_12_003750_create_flights_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('origin_city');
+            $table->string('destination_city');
+            $table->string('airline_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+};
